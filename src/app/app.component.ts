59c61608b7cecf208c2bf87f42678e45d195d181
huang, andy (ah3185) <ah3185@att.com>
2020-06-01 09:53:17 -0500

    added loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent implements OnInit {
   todo$: Observable<any>;
   todos = [];
   @ViewChild('desc', {static: false}) desc: ElementRef;
+  loading = true;
 
   constructor(
     private todoService: TodoService,
@@ -29,41 +30,52 @@ export class AppComponent implements OnInit {
     this.errorService.errorMessage = undefined;
     this.todoService.getAllTodos().subscribe(res => {
       this.todos = res;
+      this.loading = false;
     },
     error => {
       this.errorService.errorMessage = error.message ? error.message : 'error in retrieving todo';
+      this.loading = false;
     });
 
   }
 
   onEnterTodo(desc: string) {
+    this.loading = true;
     this.todoService.createTodo(desc).subscribe(res => {
       this.todos.push(res);
       this.desc.nativeElement.value = '';
+      this.loading = false;
     },
     error => {
       this.errorService.errorMessage = error.message ? error.message : 'error in creating todo';
+      this.loading = false;
     });
   }
 
   onDeleteTodo(todo: Todo) {
+    this.loading = true;
     this.todoService.deleteTodo(todo.id).subscribe(res => {
       const index = this.todos.indexOf(todo);
       if (index > -1) {
         this.todos.splice(index, 1);
       }
+      this.loading = false;
     }, error => {
       this.errorService.errorMessage = error.message ? error.message : 'error in deleting todo';
+      this.loading = false;
     });
   }
 
   onUpdateTodo(todo: Todo) {
+    this.loading = true;
     todo.isCompleted = !todo.isCompleted;
     this.todoService.updateTodo(todo).subscribe(res => {
       console.log('todo is updated', todo);
+      this.loading = false;
     },
     error => {
       this.errorService.errorMessage = error.message ? error.message : 'error in creating todo';
+      this.loading = false;
     });
   }
 }
